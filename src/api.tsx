9fcb6c0fd8f3d8b4a819a90192dc00b9a6989292
Richard Olpindo <richardolpindo@Richards-Laptop.local>
2024-05-11 13:19:11 -1000

    Initial commit. Made API calls.

--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,47 @@
-const getAllDogs = () => {
+import { Dog } from "./types";
+
+const baseURL = `http:/localhost:3000`;
+
+const getAllDogs = (): Promise<Dog[]> => {
   // fill out method
+  return fetch(`${baseURL}/dogs`).then((response) => response.json());
 };
 
-const postDog = () => {
+const postDog = (dog: Omit<Dog, "id">) => {
   // fill out method
+  fetch(`${baseURL}/dogs`, {
+    body: JSON.stringify(dog),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) throw new Error("Could not create dog");
+    return response.json();
+  });
 };
-const deleteDogRequest = () => {
+const deleteDogRequest = (id: number) => {
   // fill out method
+  fetch(`${baseURL}/dogs/${id}`, {
+    method: "DELETE",
+  }).then((response) => {
+    if (!response.ok) throw new Error("Could not delete dog");
+    return response.json();
+  });
 };
 
-const patchFavoriteForDog = () => {
+const patchFavoriteForDog = (dog: Dog) => {
   // fill out method
+  fetch(`${baseURL}/dog/${dog.id}`, {
+    body: JSON.stringify(dog),
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) throw new Error("Could not update dog.");
+    return response.json();
+  });
 };
 
 export const Requests = {
